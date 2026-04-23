c7f99d25ac488bd677630ef8bbff928b890d93a2
dinhahhh <anhdinh5991@gmail.com>
2026-04-23 22:41:48 +0700

    bai3

---
+++ bai3.js
@@ -0,0 +1,4 @@
+let width = Number(prompt(`Nhập chiều dài hình chữ nhật`));
+let height = Number(prompt(`Nhập chiều rộng hình chữ nhật`));
+console.log(`Diện tích hình chữ nhật là:` + width * height)
+document.write(`Diện tích hình chữ nhật là:` + width * height)
